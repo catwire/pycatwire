cb2d5f8b7cff54f793a7e46f16f3eb4c39f0354b
Johannes <jkbecker@bu.edu>
2020-08-25 20:52:01 -0400

    Compiling catwire/catwire@75aca71

--- catwire/ble_pdu_adv.py
+++ catwire/ble_pdu_adv.py
@@ -15,16 +15,15 @@ class BlePduAdv(KaitaiStruct):
         self._read()
 
     def _read(self):
-        self.magic = self._io.ensure_fixed_contents(b"\xD6\xBE\x89\x8E")
+        self.acc_a = self._io.ensure_fixed_contents(b"\xD6\xBE\x89\x8E")
         self.header = self._root.PduHeader(self._io, self, self._root)
         self.adv_a = self._io.read_bytes(6)
-        self.adv_data = [None] * (3)
-        for i in range(3):
-            self.adv_data[i] = self._root.AdStructure(self._io, self, self._root)
-
+        self._raw_adv_data = self._io.read_bytes((self.header.pdu_length - 6))
+        io = KaitaiStream(BytesIO(self._raw_adv_data))
+        self.adv_data = self._root.AdvData(io, self, self._root)
         self.crc = self._io.read_bytes(3)
 
-    class AdStructure(KaitaiStruct):
+    class AdData0x0a(KaitaiStruct):
         def __init__(self, _io, _parent=None, _root=None):
             self._io = _io
             self._parent = _parent
@@ -32,45 +31,10 @@ class BlePduAdv(KaitaiStruct):
             self._read()
 
         def _read(self):
-            self.adv_size = self._io.read_u1()
-            self.ad_type = self._io.read_u1()
-            _on = self.ad_type
-            if _on == 1:
-                self._raw_ad_data = self._io.read_bytes((self.adv_size - 1))
-                io = KaitaiStream(BytesIO(self._raw_ad_data))
-                self.ad_data = self._root.AdType0x01(io, self, self._root)
-            elif _on == 10:
-                self._raw_ad_data = self._io.read_bytes((self.adv_size - 1))
-                io = KaitaiStream(BytesIO(self._raw_ad_data))
-                self.ad_data = self._root.AdType0x0a(io, self, self._root)
-            elif _on == 255:
-                self._raw_ad_data = self._io.read_bytes((self.adv_size - 1))
-                io = KaitaiStream(BytesIO(self._raw_ad_data))
-                self.ad_data = self._root.AdType0xff(io, self, self._root)
-            else:
-                self._raw_ad_data = self._io.read_bytes((self.adv_size - 1))
-                io = KaitaiStream(BytesIO(self._raw_ad_data))
-                self.ad_data = self._root.AdTypeDefault(io, self, self._root)
+            self.data = self._io.read_bytes(1)
 
 
-    class PduHeader(KaitaiStruct):
-        def __init__(self, _io, _parent=None, _root=None):
-            self._io = _io
-            self._parent = _parent
-            self._root = _root if _root else self
-            self._read()
-
-        def _read(self):
-            self.pdu_type = self._io.read_bits_int(4)
-            self.rfu = self._io.read_bits_int(1) != 0
-            self.ch_sel = self._io.read_bits_int(1) != 0
-            self.tx_add = self._io.read_bits_int(1) != 0
-            self.rx_add = self._io.read_bits_int(1) != 0
-            self._io.align_to_byte()
-            self.pdu_length = self._io.read_bytes(1)
-
-
-    class AdTypeDefault(KaitaiStruct):
+    class AdDataDefault(KaitaiStruct):
         def __init__(self, _io, _parent=None, _root=None):
             self._io = _io
             self._parent = _parent
@@ -86,7 +50,7 @@ class BlePduAdv(KaitaiStruct):
 
 
 
-    class AdType0x01(KaitaiStruct):
+    class AdvData(KaitaiStruct):
         def __init__(self, _io, _parent=None, _root=None):
             self._io = _io
             self._parent = _parent
@@ -94,10 +58,15 @@ class BlePduAdv(KaitaiStruct):
             self._read()
 
         def _read(self):
-            self.data = self._io.read_bytes(1)
+            self.ad_structure = []
+            i = 0
+            while not self._io.is_eof():
+                self.ad_structure.append(self._root.AdStructure(self._io, self, self._root))
+                i += 1
 
 
-    class AdType0xff(KaitaiStruct):
+
+    class AdData0xff(KaitaiStruct):
         def __init__(self, _io, _parent=None, _root=None):
             self._io = _io
             self._parent = _parent
@@ -114,7 +83,53 @@ class BlePduAdv(KaitaiStruct):
 
 
 
-    class AdType0x0a(KaitaiStruct):
+    class AdStructure(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self._read()
+
+        def _read(self):
+            self.adv_size = self._io.read_u1()
+            self.ad_type = self._io.read_u1()
+            _on = self.ad_type
+            if _on == 1:
+                self._raw_ad_data = self._io.read_bytes((self.adv_size - 1))
+                io = KaitaiStream(BytesIO(self._raw_ad_data))
+                self.ad_data = self._root.AdData0x01(io, self, self._root)
+            elif _on == 10:
+                self._raw_ad_data = self._io.read_bytes((self.adv_size - 1))
+                io = KaitaiStream(BytesIO(self._raw_ad_data))
+                self.ad_data = self._root.AdData0x0a(io, self, self._root)
+            elif _on == 255:
+                self._raw_ad_data = self._io.read_bytes((self.adv_size - 1))
+                io = KaitaiStream(BytesIO(self._raw_ad_data))
+                self.ad_data = self._root.AdData0xff(io, self, self._root)
+            else:
+                self._raw_ad_data = self._io.read_bytes((self.adv_size - 1))
+                io = KaitaiStream(BytesIO(self._raw_ad_data))
+                self.ad_data = self._root.AdDataDefault(io, self, self._root)
+
+
+    class PduHeader(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self._read()
+
+        def _read(self):
+            self.pdu_type = self._io.read_bits_int(4)
+            self.rfu = self._io.read_bits_int(1) != 0
+            self.ch_sel = self._io.read_bits_int(1) != 0
+            self.tx_add = self._io.read_bits_int(1) != 0
+            self.rx_add = self._io.read_bits_int(1) != 0
+            self._io.align_to_byte()
+            self.pdu_length = self._io.read_u1()
+
+
+    class AdData0x01(KaitaiStruct):
         def __init__(self, _io, _parent=None, _root=None):
             self._io = _io
             self._parent = _parent
